5b60cac6242ae8a1330aff74df42b691fc4334f3
Chelsea Weaver <chelseaannweaver@gmail.com>
2016-11-30 17:32:06 -0800

    Updated notation

--- Normalize.m
+++ Normalize.m
@@ -1,18 +1,18 @@
-function [ A_norm ] = Normalize( A )
-%
-% Mon Feb 15 06:37:12 2016 written by Chelsea Weaver
-%
-% Normalize(A) normalizes the columns of the matrix A to have l2-norm equal
-% to 1.
+function [ X_norm ] = Normalize( X )
+  %
+  % Mon Feb 15 06:37:12 2016 written by Chelsea Weaver
+  %
+  % Normalize(X) normalizes the columns of the matrix X to have l2-norm equal
+  % to 1.
 
-[m,~] = size(A);
+  [m,~] = size(X);
 
-Norms = sqrt(sum(A.^2,1)); % row vector of column norms
-A_norm = A./repmat(Norms,m,1);
+  Norms = sqrt(sum(X.^2,1)); % row vector of column norms
+  X_norm = X./repmat(Norms,m,1);
 
-% Replace any NaN's with 0's:
-a = find(Norms==0);
-A_norm(:,a) = zeros(m,length(a));
+  % Replace any NaN's with 0's:
+  a = find(Norms==0);
+  X_norm(:,a) = zeros(m,length(a));
 
 end
 
