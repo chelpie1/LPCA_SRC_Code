c8d8e3c16d4599f53f6211a45cd72fd327a16a05
Chelsea Weaver <chelseaannweaver@gmail.com>
2016-11-30 17:07:00 -0800

    Update LPCA_SRC.m

--- LPCA_SRC.m
+++ LPCA_SRC.m
@@ -27,12 +27,12 @@
 %% USER INPUT:
 
 % Load data. Must specify:
-    % A_train (m x n_train matrix of training samples)
-    % A_test (m x n_test matrix of test samples)
+    % X_train (m x n_train matrix of training samples)
+    % X_test (m x n_test matrix of test samples)
     % lab (training label vector)
     % ulab_GT (ground truth label of test samples)
     
-% tot_num = 100; K = 8; noise_level = 0.01;
+% tot_num = 100; L = 8; noise_level = 0.01;
 % run Data_Gen_Spherical_Sine % My synthetic data set
     lab = LAB;
     ulab_GT = ULAB_GT;
@@ -56,15 +56,15 @@ occ_on = 0; % 0 ~ no occlusion, 1 ~ occlusion
 % How is the intrinsic class parameter d set?
 d_opt = 0; % 0 ~ manually, % 1 ~ per class using DANCo algorithm.
 
-% if d_opt == 0
-%     % Manually specify d_vect as a Kx1 dimensional vector of intrinsic
-%     % class dimensions:
-%     d_vect = ones(K,1);
-% end
+if d_opt == 0
+     % Manually specify d_vect as a Lx1 dimensional vector of intrinsic
+     % class dimensions:
+     d_vect = ones(L,1);
+end
 
 % Number of neighbors in Local PCA:
 % number of neighbors must be >= d and <= min(quant_train)-2
-%n = 3;
+n = 3;
 
 tic
 
@@ -72,38 +72,38 @@ tic
 
 % Extract information from training data:
 
-[m,n_train] = size(A_train);
+[m,n_tr] = size(X_tr);
 
-K = length(unique(lab));
+L = length(unique(lab));
 
-quant_train = zeros(K,1);
-for l=1:K
+quant_train = zeros(L,1);
+for l=1:L
     quant_train(l) = nnz(lab==l);
 end
 
 % Normalize training data:
-A_train_norm = Normalize(A_train);
+X_tr_norm = Normalize(X_tr);
 
 % Set d_vect if needed:
 if d_opt == 1; % if using an estimation algorithm:
-    d_vect = zeros(K,1);
-    for l=1:K
+    d_vect = zeros(L,1);
+    for l=1:L
         class_l_index = lab==l;
-        A_train_l = A_train(:,class_l_index);
+        X_tr_l = X_tr(:,class_l_index);
         d_vect(l) = DANCo(A_train_l);
     end
 end
 
 % Stack training data by class:
-A_train_stacked = zeros(m,max(quant_train),K);
-for l=1:K
+X_tr_stacked = zeros(m,max(quant_train),L);
+for l=1:L
     class_l_index = lab==l; 
-    A_train_stacked(:,1:quant_train(l),l) = A_train_norm(:,class_l_index);
+    X_tr_stacked(:,1:quant_train(l),l) = X_tr_norm(:,class_l_index);
 end
 
 % Compute tangent vectors and neighborhood radius parameter r:
 [ DICT_full_stacked, r_1] = ...
-    Local_PCA( A_train_stacked, quant_train, d_vect, n );
+    Local_PCA( X_tr_stacked, quant_train, d_vect, n );
 
 % Write DICT_full_stacked as a 2D matrix and compute label vector as well
 % as an index vector of training points in DICT_full:
@@ -111,11 +111,11 @@ DICT_full = zeros(m,dot(quant_train,(d_vect+1)));
 lab_DICT_full = zeros(1,dot(quant_train,(d_vect+1)));
 train_pt_ind = zeros(n_train,1); 
 % Note that lab_DICT_full(train_pt_ind(i)) will retrieve the class of the
-% ith training point in A_train.
+% ith training point in X_tr.
 
 count_1 = 1;
 count_2 = 1;
-for l=1:K
+for l=1:L
     
     DICT_full(:,count_1:count_1+quant_train(l)*(d_vect(l)+1)-1) = ...
         DICT_full_stacked(:,1:quant_train(l)*(d_vect(l)+1),l);
@@ -134,44 +134,44 @@ DICT_full = Normalize(DICT_full);
 
 % Extract information from test data:
 
-[~,n_test] = size(A_test);
+[~,n_te] = size(X_te);
 
 % Normalize test data:
-A_test_norm = Normalize(A_test);
+X_te_norm = Normalize(X_te);
 
 % Compute average number of columns in dictionary after r constraint:
-len_DICT = zeros(n_test,1);
+len_DICT = zeros(n_te,1);
 
 % Store HOMOTOPY statistics:
 if alg_opt == 1
     % Number of iterations in HOMOTOPY algorithm:
-    ITER = zeros(n_test,1);
+    ITER = zeros(n_te,1);
     % Computation time of HOMOTOPY algorithm:
-    TIME = zeros(n_test,1);
+    TIME = zeros(n_te,1);
 end
 
 % Store sparsity of coefficient vector:
-SPAR = zeros(n_test,1);
+SPAR = zeros(n_te,1);
 
 % Check variance in classes represented in D_y:
-VAR = zeros(n_test,1);
+VAR = zeros(n_te,1);
 
 % Initialize test label vector:
-ulab_LPCA_SRC = zeros(n_test,1);
+ulab_LPCA_SRC = zeros(n_te,1);
 
 % Begin classification:
 
-for j=1:n_test
-    y = A_test_norm(:,j);
+for j=1:n_te
+    y = X_te_norm(:,j);
     
     DICT_y = zeros(size(DICT_full));
     lab_DICT_y = zeros(length(lab_DICT_full));
     
     % Compute distances between the test point and each training point:
-    dist_vects_pos = repmat(y,1,n_train)-A_train_norm;
+    dist_vects_pos = repmat(y,1,n_tr)-X_tr_norm;
     DIST_pos = sqrt(sum(dist_vects_pos.^2,1))';
     
-    dist_vects_neg = repmat(y,1,n_train)+A_train_norm;
+    dist_vects_neg = repmat(y,1,n_tr)+X_tr_norm;
     DIST_neg = sqrt(sum(dist_vects_neg.^2,1))';
     
     % Compute minimum distance from y to a class rep
@@ -249,8 +249,8 @@ for j=1:n_test
     end
     
     % Compute class error for each class:
-    ERR_y = zeros(1,K);
-    for l=1:K
+    ERR_y = zeros(1,L);
+    for l=1:L
         coeff = zeros(len_DICT(j),1);
         ind_l = find(lab_DICT_y==l);
         if isempty(ind_l)==0 % if DICT_j contains vectors from class l
@@ -272,7 +272,7 @@ end
 
 %% Compute accuracy:
 
-ACC_LPCA_SRC = nnz(ulab_LPCA_SRC == ulab_GT)/n_test;
+ACC_LPCA_SRC = nnz(ulab_LPCA_SRC == ulab_GT)/n_te;
 
 %% Compute statistics:
 
@@ -291,43 +291,3 @@ Avg_Hom_Time = mean(TIME);
 % Average sparsity of l1-minimized coefficient vector x:
 Avg_Sparsity = mean(SPAR);
 
-Var_in_DICT_y = mean(VAR);
-
-%% Optional: Plot classification results:
-
-%     DATA = A;
-%     Y = DATA';
-% 
-%     Y_train = Y(1:LB,1:3);
-%     Y_test = Y(LB+1:LB+ULB,1:3);
-% 
-%     % Plot correctly classified points (blue):
-%     CORRECT = find(ulab_LPCA_SRC == ULAB_GT);
-%     CORRECT_COORDS = Y_test(CORRECT,:);
-%     x1 = CORRECT_COORDS(:,1); y1 = CORRECT_COORDS(:,2);
-%     z1=CORRECT_COORDS(:,3);
-% 
-%     scatter3(x1,y1,z1,'k');
-% 
-%     % Plot incorrectly classified points (red):
-%     INCORRECT = find(ulab_LPCA_SRC ~= ULAB_GT);
-%     INCORRECT_COORDS = Y_test(INCORRECT,:);
-%     x2 = INCORRECT_COORDS(:,1); y2 = INCORRECT_COORDS(:,2);
-%     z2 = INCORRECT_COORDS(:,3);
-% 
-%     hold on
-%     scatter3(x2,y2,z2,'k','filled');
-%     
-%     % Plot tangent vectors:
-%     for l=1:K
-%         class = find(lab_DICT_full==l);
-%         DICT_full_class = DICT_full(:,class);
-%         Tan_Vects_class = DICT_full_class(:,1:d_vect(1)+1:end);
-%         hold on
-%         scatter3(Tan_Vects_class(1,:),Tan_Vects_class(2,:),Tan_Vects_class(3,:),colors(l),'d','filled');
-%     end
-    
-    
-    
-    
-    
